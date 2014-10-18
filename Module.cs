5ecc37e3b97566bff67457195636388b6b9bdd07
shuzhen <sue10th@hotmail.com>
2014-10-18 14:53:27 +0800

    module change to new Data Type

--- Module.cs
+++ Module.cs
@@ -1,12 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using PAT.Common.Classes.Expressions.ExpressionClass;
+
 //the namespace must be PAT.Lib, the class and method names can be arbitrary
 namespace PAT.Lib
 {
+    /// <summary>
+    /// The math library that can be used in your model.
+    /// all methods should be declared as public static.
+    /// 
+    /// The parameters must be of type "int", or "int array"
+    /// The number of parameters can be 0 or many
+    /// 
     /// The return type can be bool, int or int[] only.
-
-    public class Module
+    /// 
+    /// The method name will be used directly in your model.
+    /// e.g. call(max, 10, 2), call(dominate, 3, 2), call(amax, [1,3,5]),
+    /// 
+    /// Note: method names are case sensetive
+    /// </summary>
+    public class Module : ExpressionValue
     {
         private int classSize;
         private int ta;
@@ -64,6 +78,37 @@ namespace PAT.Lib
             assignedSlot = startSlot;
         }
 
+
+    
+
+        /// <summary>
+        /// Please implement this method to provide the string representation of the datatype
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "";
+        }
+
+
+        /// <summary>
+        /// Please implement this method to return a deep clone of the current object
+        /// </summary>
+        /// <returns></returns>
+        public override ExpressionValue GetClone()
+        {
+            return this;
+        }
+
+
+        /// <summary>
+        /// Please implement this method to provide the compact string representation of the datatype
+        /// </summary>
+        /// <returns></returns>
+        public override string ExpressionID
+        {
+            get {return ""; }
+        }
 
     }
 }
