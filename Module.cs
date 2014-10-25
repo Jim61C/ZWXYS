d5b586a4a17f7b36e25f2bf0ed0148b42c9e87ef
shuzhen <sue10th@hotmail.com>
2014-10-25 12:43:56 +0800

    update module

--- Module.cs
+++ Module.cs
@@ -1,26 +1,37 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using PAT.Common.Classes.Expressions.ExpressionClass;
+
 //the namespace must be PAT.Lib, the class and method names can be arbitrary
 namespace PAT.Lib
 {
+    /// <summary>
+    /// The math library that can be used in your model.
+    /// all methods should be declared as public static.
+    /// 
+    /// The parameters must be of type "int", or "int array"
+    /// The number of parameters can be 0 or many
+    /// 
     /// The return type can be bool, int or int[] only.
-
-    public class Module
+    /// 
+    /// The method name will be used directly in your model.
+    /// e.g. call(max, 10, 2), call(dominate, 3, 2), call(amax, [1,3,5]),
+    /// 
+    /// Note: method names are case sensetive
+    /// </summary>
+    public class Module: ExpressionValue
     {
         private int classSize;
         private int ta;
         private bool gradClass;
         private int tutLen; // length of tutorial eg. 3 if tutorial takes up 3 timeslots
         private int assignedSlot;
+        private int tutRMLoc;
 
         public Module()
         {
-            this.classSize = 0;
-            this.ta = 0;
-            this.gradClass = false;
-            this.tutLen = 0;
-            this.assignedSlot = 0;
+        
         }
 
         public Module(int classSize, int ta, bool gradClass, int tutLen)
@@ -58,12 +69,53 @@ namespace PAT.Lib
             return ta;
         }
 
+        public int getTutRMLoc()
+        {
+            return tutRMLoc;
+        }
+
         //setters
         public void setAssignedSlot(int startSlot)
         {
             assignedSlot = startSlot;
         }
 
+        public void setTutRoomLoc(int loc)
+        {
+            tutRMLoc = loc;
+        }
+
+
+    
+
+        /// <summary>
+        /// Please implement this method to provide the string representation of the datatype
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "";
+        }
+
+
+        /// <summary>
+        /// Please implement this method to return a deep clone of the current object
+        /// </summary>
+        /// <returns></returns>
+        public override ExpressionValue GetClone()
+        {
+            return this;
+        }
+
+
+        /// <summary>
+        /// Please implement this method to provide the compact string representation of the datatype
+        /// </summary>
+        /// <returns></returns>
+        public override string ExpressionID
+        {
+            get {return ""; }
+        }
 
     }
 }
